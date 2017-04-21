2c0dd7c0086506374c1b608fd6db5fd4e1e251ba
Steven Tobin <stobin29.st@gmail.com>
2017-04-21 15:45:47 +0100

    added pricing screen

--- AWSApp.py
+++ AWSApp.py
@@ -8,6 +8,7 @@ from kivy.factory import Factory
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+import decimal
 import collections
 import time
 import datetime
@@ -94,6 +95,33 @@ class HomeScreen(Screen):
 
 class PluginsScreen(Screen):
     text = StringProperty('')
+    priceLayout = ObjectProperty(None)
+
+    def doConvertRegion(self, region):
+        if region == "us-east-1":
+            return "US East (N. Virginia)"
+        if region == "us-east-2":
+            return "US East (Ohio)"
+        if region == "us-west-1":
+            return "US West (N. California)"
+        if region == "us-west-2":
+            return "US West (Oregon)"
+        if region == "ap-south-1":
+            return "Asia Pacific (Mumbai)"
+        if region == "ap-northeast-2":
+            return "Asia Pacific (Seoul)"
+        if region == "ap-southeast-1":
+            return "Asia Pacific (Singapore)"
+        if region == "ap-southeast-2":
+            return "Asia Pacific (Sydney)"
+        if region == "ap-northeast-1":
+            return "Asia Pacific (Tokyo)"
+        if region == "eu-central-1":
+            return "EU (Frankfurt)"
+        if region == "eu-west-1":
+            return "EU (Ireland)"
+        if region == "eu-west-2":
+            return "EU (London)"
 
     # overwrite the switch_to function to control the
     #   screenmanager from here
@@ -108,6 +136,127 @@ class PluginsScreen(Screen):
         for p in plugs:
             ret += p + "\n"
         self.text = ret
+
+    def filter_products(self, products):
+        filtered = []
+
+        items = products['products'].items()
+
+        # Only interested in shared tenancy, linux instances
+        for sku, product in items:
+            a = product['attributes']
+            if not ('locationType' in a and
+                            'location' in a and
+                            'tenancy' in a and
+                            a['tenancy'] == "Shared" and
+                            a['locationType'] == 'AWS Region' and
+                            a['operatingSystem'] == 'Linux'):
+                continue
+
+            a['sku'] = sku
+            filtered.append(a)
+
+        return filtered
+
+
+    def getPrices(self, filtered, terms):
+        instances = {}
+        for a in filtered:
+            k = list(terms['OnDemand'][a['sku']])[0]
+            pdKey = list(terms['OnDemand'][a['sku']][k]['priceDimensions'])[0]
+            cost = float(terms['OnDemand'][a['sku']][k]['priceDimensions'][pdKey]['pricePerUnit']['USD'])
+
+            info = {"type": a['instanceType'], "vcpu": a['vcpu'],
+                    "memory": a['memory'].split(" ")[0], "cost": cost}
+
+            if not a['location'] in instances:
+                instances[a['location']] = []
+
+            instances[a['location']].append(info)
+
+        return instances
+
+    def buildTable(self):
+
+        self.showpopup()
+        self.pop_up.value = 0
+        self.pop_up.update_pop_up_info('Collecting Price Information')
+
+        with open('data/prices', 'r') as openfile:
+            prices = json.load(openfile)
+
+        filter = self.filter_products(prices)
+        regionPrices = self.getPrices(filter, prices['terms'])
+
+        print(regionPrices['EU (Ireland)'])
+
+        self.priceLayout.clear_widgets()
+
+        # Build first row
+        self.priceLayout.add_widget(Label(text="Region", size_hint=(1, None), height=40))
+        self.priceLayout.add_widget(Label(text="Number", size_hint=(1, None), height=40))
+        self.priceLayout.add_widget(Label(text="Price", size_hint=(1, None), height=40))
+        self.priceLayout.add_widget(Label(text="Total Per Day", size_hint=(1, None), height=40))
+
+        # Find regions
+        regions = core.doReadRegions()
+
+        self.pop_up.update_pop_up_info('Building overview')
+        priceLayout = self.priceLayout
+
+        # In each of the following it is necessary to refresh the size of the table after adding
+        # an item (tableLayout.height=self.tableLayout.minimum_height)
+
+        total = decimal.Decimal(0.0)
+        totalCount = 0
+
+        for reg in regions:
+            self.pop_up.update_pop_up_text('Current Region: ' + str(reg))
+            self.priceLayout.add_widget(Label(text=reg, size_hint=(1, None), height=40))
+            priceLayout.height=self.priceLayout.minimum_height
+
+            instCount = 0
+            regCost = decimal.Decimal(0.0)
+
+            # Count ec2 instances
+            for e in ec2Data:
+                if reg in e:
+                    convRegion = self.doConvertRegion(reg)
+                    k = len(e[reg].keys())
+                    for inst in e[reg]:
+                        instCount += 1
+                        totalCount += 1
+                        instanceType = e[reg][inst]['Type']
+                        for c in regionPrices[convRegion]:
+                            if c['type'] == instanceType:
+                                cost = decimal.Decimal(c['cost'])
+                                total = total + cost
+                                regCost = regCost + cost
+
+            regTotal = decimal.Decimal(regCost * 24)
+            quantRegTotal = regTotal.quantize(decimal.Decimal('.01'), decimal.ROUND_HALF_UP)
+            quantRegCost = regCost.quantize(decimal.Decimal('.01'), decimal.ROUND_HALF_UP)
+
+            self.priceLayout.add_widget(Label(text=str(instCount), size_hint=(1, None), height = 40))
+            self.priceLayout.add_widget(Label(text=str("$"+str(quantRegCost)), size_hint=(1, None), height=40))
+            self.priceLayout.add_widget(Label(text=str("$"+str(quantRegTotal)), size_hint=(1,None), height=40))
+            priceLayout.height = self.priceLayout.minimum_height
+
+        total = decimal.Decimal(total * 24)
+        quantTotal = total.quantize(decimal.Decimal('.01'), decimal.ROUND_HALF_UP)
+
+        self.priceLayout.add_widget(Label(text="Total", size_hint=(1, None), height=40))
+        self.priceLayout.add_widget(Label(text=str(totalCount), size_hint=(1, None), height=40))
+        self.priceLayout.add_widget(Label(text=" ", size_hint=(1, None), height=40))
+        self.priceLayout.add_widget(Label(text=str("$"+str(quantTotal)), size_hint=(1, None), height=40))
+        priceLayout.height = self.priceLayout.minimum_height
+
+        self.pop_up.dismiss()
+
+    def showpopup(self):
+        self.pop_up = Factory.PopupBox()
+        self.pop_up.update_pop_up_text('Connecting...')
+        self.pop_up.open()
 
 class MyScreenManager(ScreenManager):
     pass
@@ -212,50 +361,54 @@ class MainWindow(GridLayout):
         print(loadPerTick)
         self.pop_up.value = barValue
 
-        for p in profiles:
-            count = 1
-            for region in regions:
-                self.pop_up.update_pop_up_text('Current Region: '+str(region)+" ("+str(count)+"/"+str(tReg)+")")
-                session = boto3.Session(profile_name=p, region_name=region)
-                resEc2 = {}
-                resEbs = {}
-                self.pop_up.update_pop_up_info('Collecting Infrastructure')
-                core.doCollectResources(session, region, resources)
+        try:
+            for p in profiles:
+                count = 1
+                for region in regions:
+                    self.pop_up.update_pop_up_text('Current Region: '+str(region)+" ("+str(count)+"/"+str(tReg)+")")
+                    session = boto3.Session(profile_name=p, region_name=region)
+                    resEc2 = {}
+                    resEbs = {}
+                    self.pop_up.update_pop_up_info('Collecting Infrastructure')
+                    core.doCollectResources(session, region, resources)
 
-                #
-                # Information Gathering
-                #
+                    #
+                    # Information Gathering
+                    #
 
-                # ec2Data
-                self.pop_up.update_pop_up_info('Collecting EC2 instances')
-                currInst = core.doFindEC2Information(session, region)
-                resEc2[region] = currInst
-                ec2Data.append(resEc2)
+                    # ec2Data
+                    self.pop_up.update_pop_up_info('Collecting EC2 instances')
+                    currInst = core.doFindEC2Information(session, region)
+                    resEc2[region] = currInst
+                    ec2Data.append(resEc2)
 
 
-                # volData
-                self.pop_up.update_pop_up_info('Collecting EBS volumes')
-                currVol = core.doFindEBSInformation(session, region)
-                barValue += loadPerTick
-                self.pop_up.setValue(barValue)
-                resEbs[region] = currVol
-                ebsData.append(resEbs)
-                count += 1
+                    # volData
+                    self.pop_up.update_pop_up_info('Collecting EBS volumes')
+                    currVol = core.doFindEBSInformation(session, region)
+                    barValue += loadPerTick
+                    self.pop_up.setValue(barValue)
+                    resEbs[region] = currVol
+                    ebsData.append(resEbs)
+                    count += 1
 
-            # s3Data
-            session = boto3.Session(profile_name=p)
-            self.pop_up.update_pop_up_info('Collecting S3 buckets')
-            s3Data = core.doFindS3Information(session)
+                # s3Data
+                session = boto3.Session(profile_name=p)
+                self.pop_up.update_pop_up_info('Collecting S3 buckets')
+                s3Data = core.doFindS3Information(session)
 
-        self.pop_up.update_pop_up_info("Analysing data..")
-        self.pop_up.update_pop_up_text("Please wait...")
-        pluginManager.doRunPlugins(ec2Data, s3Data, ebsData, resources)
+            self.pop_up.update_pop_up_info("Analysing data..")
+            self.pop_up.update_pop_up_text("Please wait...")
+            pluginManager.doRunPlugins(ec2Data, s3Data, ebsData, resources)
 
-        # Popup
-        self.pop_up.update_pop_up_text("Complete!")
-        self.pop_up.setValue(100)
-        self.pop_up.update_pop_up_info("")
-        self.pop_up.dismiss()
+            # Popup
+            self.pop_up.update_pop_up_text("Complete!")
+            self.pop_up.setValue(100)
+            self.pop_up.update_pop_up_info("")
+            self.pop_up.dismiss()
+        except:
+            print("failed fetching data")
+            self.pop_up.update_pop_up_text("Failed Fetching data")
 
     def doSaveSnapshot(self):
         ts = time.time()
@@ -282,6 +435,15 @@ class MainWindow(GridLayout):
         self.pop_up = Factory.PopupBox()
         self.pop_up.update_pop_up_text('Connecting...')
         self.pop_up.open()
+
+    def doBuildTable(self):
+        if self.children[0].current == "home":
+            self.children[0].get_screen('home').buildTable()
+        if self.children[0].current == "plugins":
+            self.children[0].get_screen('plugins').buildTable()
+
+    def doExit(self):
+        App.get_running_app().stop()
 
 class PopupBox(Popup):
     pop_up_text = ObjectProperty()
